0517e9dcf6d91e32ee8b75bd728986fd3ca145d9
piegeek <piegeek2@naver.com>
2019-12-31 18:10:00 +0900

    db READ implemented

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-let db;
-
-// DATABASE RELATED STRINGS
-const databaseUri = 'mongodb://localhost:27017';
-const databaseName = 'SnuScraperLocalTest' 
 
 // CREATE APP
 const app = express();
@@ -16,18 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 // ROUTE SETUP
 app.use('/api/lectures', require('./routes/api/lectures'));
 
-// DATABASE CONNECTION AND PORT SETUP
+// PORT SETUP
 const PORT = process.env.PORT || 5000;
-
-MongoClient.connect(databaseUri, {useUnifiedTopology: true}, (err, client) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    db = client.db(databaseName);
-    app.listen(PORT, () => console.log('Server Started'));
-});
-
-console.log(db);
-
-module.exports = db;
+app.listen(PORT, () => console.log('Server Started'));
--- routes/api/lectures.js
+++ routes/api/lectures.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../../index');
+const MongoClient = require('mongodb').MongoClient;
+let db;
 
-router.get('/:title', (req, res) => {
-    console.log(db);
-    lectures = db.lectures.find(
-        {
-            '교과목명': parseString(req.params.title)
-        }
-    );
-    res.json(lectures);    
+// DATABASE RELATED STRINGS
+const databaseUri = 'mongodb://localhost:27017';
+const databaseName = 'SnuScraperLocalTest' 
+
+// DATABASE CONNECTION
+MongoClient.connect(databaseUri, {useUnifiedTopology: true}, (err, client) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    db = client.db(databaseName);
 });
 
-router.get('/:number', (req, res) => {
+router.get('/title/:title', (req, res) => {
+    db.collection('lectures').find({ '교과목명': req.params.title }).toArray((err, items) => {
+        res.json(items);
+    });
+});
 
+router.get('/code/:code', (req, res) => {
+    db.collection('lectures').find({ '교과목번호': req.params.code }).toArray((err, items) => {
+        res.json(items);
+    });
 });
 
 module.exports = router;
