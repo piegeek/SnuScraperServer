3bc029595ccba42483a0870aa905917db23ad85b
piegeek <piegeek2@naver.com>
2020-01-05 12:38:36 +0900

    Post request handling

--- routes/api/lectures.js
+++ routes/api/lectures.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 let db;
 
 // DATABASE RELATED STRINGS
 const databaseUri = 'mongodb://localhost:27017';
-const databaseName = 'SnuScraperLocalTest' 
+const databaseName = 'SSLT2019WINTER' 
 
 // DATABASE CONNECTION
 MongoClient.connect(databaseUri, {useUnifiedTopology: true}, (err, client) => {
@@ -26,6 +27,33 @@ router.get('/code/:code', (req, res) => {
     db.collection('lectures').find({ '교과목번호': req.params.code }).toArray((err, items) => {
         res.json(items);
     });
+});
+
+router.post('/', (req, res) => {
+    db.collection('lectures').updateOne(
+        { '_id': new ObjectID(req.body.lectureId) },
+        { '$addToSet': { 'users': String(req.body.userId) } },
+    )
+    .then(() => {
+        return res.json({ success: true });
+    })
+    .catch(err => {
+        console.log(`Error while adding user('id': ${req.body.userId}) to lecture('id': ${req.body.lectureId}) `);
+    })
+
+});
+
+router.post('/delete/', (req, res) => {
+    db.collection('lectures').updateOne(
+        { '_id': new ObjectID(req.body.lectureId) },
+        { '$pull': { 'users': String(req.body.userId) } }
+    )
+    .then(() => {
+        return res.json({ success: true });
+    })
+    .catch(err => {
+        console.log(`Error while deleting user('id': ${req.body.userId}) from lecture('id': ${req.body.lectureId}) `);
+    })
 });
 
 module.exports = router;
