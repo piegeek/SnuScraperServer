6bba95253c1810cf63ea22eabef9010dcca3a673
piegeek <piegeek2@naver.com>
2020-01-24 19:23:44 +0900

    Logging added

--- routes/api/lectures.js
+++ routes/api/lectures.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
+const winston = require('winston');
 let db;
 
 // DATABASE RELATED STRINGS
@@ -16,38 +17,60 @@ MongoClient.connect(databaseUri, {useUnifiedTopology: true}, (err, client) => {
     db = client.db(databaseName);
 });
 
+// CREATE LOGGER
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.File({ filename: 'log/error/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'log/activity/combined.log' })
+    ]
+}); 
+
 router.get('/title/:title', async (req, res) => {
-    try {
-        let lectures = await db.collection('lectures').find({ '교과목명': req.params.title }).toArray();
-        if (lectures.length === 0) {
+    try {                        
+        const lectures = await db.collection('lectures').find().toArray();
+        
+        const cleanedTitleData = req.params.title.split(' ').join('');
+        lecturesToSend = lectures.filter(item => item['교과목명'].split(' ').join('') === cleanedTitleData);
+
+        if (lecturesToSend.length === 0) {
+            logger.log({ level: 'error', message: `Fail for TITLE: ${req.params.title}, CLEANED_TITLE: ${cleanedTitleData}` });
             res.sendStatus(400);
         }
         else {
-            res.json(lectures);
+            logger.log({ level: 'info', message: `Success for TITLE: ${req.params.title}` });
+            res.json(lecturesToSend);
         }
     }
     catch(err) {
+        logger.log({ level: 'error', message: `DATABASE ERROR! CHECK IF SERVER IS CONNECTED TO THE DATABASE.` });
         res.sendStatus(400);
     }
 });
 
 router.get('/code/:code', async (req, res) => {
     try {
-        let lectures = await db.collection('lectures').find({ '교과목번호': req.params.code }).toArray();
+        const cleanedCodeData = req.params.code.split(' ').join('').toUpperCase();
+        
+        const lectures = await db.collection('lectures').find({ '교과목번호': cleanedCodeData }).toArray();
         if (lectures.length === 0) {
+            logger.log({ level: 'error', message: `Fail for CODE: ${req.params.code}, CLEANED_CODE: ${cleanedCodeData}` });
             res.sendStatus(400);
         }
         else {
+            logger.log({ level: 'info', message: `Success for CODE: ${req.params.code}` });
             res.json(lectures);
         }
     }
     catch(err) {
+        logger.log({ level: 'error', message: `DATABASE ERROR! CHECK IF SERVER IS CONNECTED TO THE DATABASE.` });
         res.sendStatus(400);
     }
-    
-    db.collection('lectures').find({ '교과목번호': req.params.code }).toArray((err, items) => {
-        res.json(items);
-    });
 });
 
 router.post('/', async (req, res) => {
@@ -56,11 +79,13 @@ router.post('/', async (req, res) => {
             { '_id': new ObjectID(req.body.lectureId) },
             { '$addToSet': { 'users': String(req.body.userId) } },
         );
+        
+        logger.log({ level: 'info', message: `Successfully added user('id': ${req.body.userId}) to lecture('id': ${req.body.lectureId})` });
         res.sendStatus(200);
     }
     catch(err) {
-        console.log(`Error while adding user('id': ${req.body.userId}) to lecture('id': ${req.body.lectureId}) `);
-        console.log(`Error message: ${err}`);
+        logger.log({ level: 'error', message: `Error while adding user('id': ${req.body.userId}) to lecture('id': ${req.body.lectureId})` });
+        logger.log({ level: 'error', message: `Error message: ${err}` });
         res.sendStatus(400);
     }
 });
@@ -71,11 +96,13 @@ router.post('/delete/', async (req, res) => {
             { '_id': new ObjectID(req.body.lectureId) },
             { '$pull': { 'users': String(req.body.userId) } }
         );
+
+        logger.log({ level: 'info', message: `Successfully deleted user('id': ${req.body.userId}) from lecture('id': ${req.body.lectureId})` });
         res.sendStatus(200);
     }
     catch(err) {
-        console.log(`Error while deleting user('id': ${req.body.userId}) from lecture('id': ${req.body.lectureId}) `);
-        console.log(`Error message: ${err}`);
+        logger.log({ level: 'error', message: `Error while deleting user('id': ${req.body.userId}) from lecture('id': ${req.body.lectureId})` });
+        logger.log({ level: 'error', message: `Error message: ${err}` });        
         res.sendStatus(400);
     }
 });
