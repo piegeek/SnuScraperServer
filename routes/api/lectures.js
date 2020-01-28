0eb22aa519ab99f7135eece64fd3828e9a1f9ed6
Sang Yeop Han <piegeek@Sangs-MacBook-Pro.local>
2020-01-29 03:07:19 +0900

    Changed log file path

--- routes/api/lectures.js
+++ routes/api/lectures.js
@@ -28,8 +28,8 @@ const logger = winston.createLogger({
     ),
     defaultMeta: { service: 'user-service' },
     transports: [
-        new winston.transports.File({ filename: '../../log/error/error.log', level: 'error' }),
-        new winston.transports.File({ filename: '../../log/activity/combined.log' })
+        new winston.transports.File({ filename: './../../log/error/error.log', level: 'error' }),
+        new winston.transports.File({ filename: './../../log/activity/combined.log' })
     ]
 }); 
 
